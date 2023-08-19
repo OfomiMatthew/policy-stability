6f30874b03e8b40b71ee70951a7571a58422d4dd
OfomiMatthew <ofomimatthew7@gmail.com>
2023-08-19 17:34:21 +0100

    update on static files

--- policyCheckProject/policyApp/static/js/script.js
+++ policyCheckProject/policyApp/static/js/script.js
@@ -1,109 +1,109 @@
-let submit = document.querySelector('.submit')
-let btn = document.querySelector('.click-btn')
-let stable = document.querySelector('.stable')
-let unstable = document.querySelector('.unstable')
-let body = document.querySelector('body')
-let calc_value = document.querySelector('.calc_value')
-let check = document.getElementsByClassName("check")
+// let submit = document.querySelector('.submit')
+// let btn = document.querySelector('.click-btn')
+// let stable = document.querySelector('.stable')
+// let unstable = document.querySelector('.unstable')
+// let body = document.querySelector('body')
+// let calc_value = document.querySelector('.calc_value')
+// let check = document.getElementsByClassName("check")
 
-let resultNote = document.querySelector('.result-note')
-let divStable = document.createElement('div')
-let pStable = document.createElement('p')
-let actionStable = document.createElement('h5')
-let noteStable = document.createElement('p')
-
-
-
-
-// formula for psc to be calculated
-let total_yes_length = document.querySelectorAll('input[value="Yes"]').length;
-let total_no_length = document.querySelectorAll('input[value="No"]').length
-
-btn.addEventListener('click',()=>{
-  submit.style.display = 'block'
-  let yes_count = 0;
-  let no_count = 0;
-  for(let i = 0; i < check.length; i++){
-    if(check[i].value ==='Yes' && check[i].checked){
-      yes_count++
-    }else if(check[i].value ==='No' && check[i].checked){
-      no_count++
-    }
-  }
-
-let numerator_value = 7 + (yes_count - no_count)
-let denominator_value = 1 + (total_yes_length + total_no_length)
-
-let final_value = Math.ceil((numerator_value / denominator_value) * 100)
-console.log(final_value)
-calc_value.textContent = `Score: ${(final_value)}%`
+// let resultNote = document.querySelector('.result-note')
+// let divStable = document.createElement('div')
+// let pStable = document.createElement('p')
+// let actionStable = document.createElement('h5')
+// let noteStable = document.createElement('p')
 
 
 
-if(final_value >=70){
-  // stable.style.display = 'block';
-  pStable.innerText = "Stable policy choice"
-  actionStable.innerText = "Action"
-  noteStable.innerText = "May proceed with implementation"
+
+// // formula for psc to be calculated
+// let total_yes_length = document.querySelectorAll('input[value="Yes"]').length;
+// let total_no_length = document.querySelectorAll('input[value="No"]').length
+
+// btn.addEventListener('click',()=>{
+//   submit.style.display = 'block'
+//   let yes_count = 0;
+//   let no_count = 0;
+//   for(let i = 0; i < check.length; i++){
+//     if(check[i].value ==='Yes' && check[i].checked){
+//       yes_count++
+//     }else if(check[i].value ==='No' && check[i].checked){
+//       no_count++
+//     }
+//   }
+
+// let numerator_value = 7 + (yes_count - no_count)
+// let denominator_value = 1 + (total_yes_length + total_no_length)
+
+// let final_value = Math.ceil((numerator_value / denominator_value) * 100)
+// console.log(final_value)
+// calc_value.textContent = `Score: ${(final_value)}%`
+
+
+
+// if(final_value >=70){
+//   // stable.style.display = 'block';
+//   pStable.innerText = "Stable policy choice"
+//   actionStable.innerText = "Action"
+//   noteStable.innerText = "May proceed with implementation"
   
-  divStable.appendChild(pStable);
-  divStable.appendChild(actionStable);
-  divStable.appendChild(noteStable);
-  divStable.style.backgroundColor ="#5cc019"
-  divStable.style.border ="1px solid none"
-  divStable.style.color ="whitesmoke"
-  divStable.style.fontWeight ="400"
-  divStable.style.borderRadius ="5px"
-  divStable.style.padding ="7px"
-  resultNote.appendChild(divStable)
-}else{
-  pStable.innerText = "Unstable policy choice"
-  actionStable.innerText = "Action"
-  noteStable.innerText = "Refine decision or try other options"
+//   divStable.appendChild(pStable);
+//   divStable.appendChild(actionStable);
+//   divStable.appendChild(noteStable);
+//   divStable.style.backgroundColor ="#5cc019"
+//   divStable.style.border ="1px solid none"
+//   divStable.style.color ="whitesmoke"
+//   divStable.style.fontWeight ="400"
+//   divStable.style.borderRadius ="5px"
+//   divStable.style.padding ="7px"
+//   resultNote.appendChild(divStable)
+// }else{
+//   pStable.innerText = "Unstable policy choice"
+//   actionStable.innerText = "Action"
+//   noteStable.innerText = "Refine decision or try other options"
   
-  divStable.appendChild(pStable);
-  divStable.appendChild(actionStable);
-  divStable.appendChild(noteStable);
-  divStable.style.backgroundColor ="#ff4500"
-  divStable.style.border ="1px solid none"
-  divStable.style.color ="whitesmoke"
-  divStable.style.fontWeight ="400"
-  divStable.style.borderRadius ="5px"
-  divStable.style.padding ="7px"
-  resultNote.appendChild(divStable)
-}
- console.log(`yes: ${yes_count}, no:${no_count}`)
- getChart(yes_count,no_count)
-})
+//   divStable.appendChild(pStable);
+//   divStable.appendChild(actionStable);
+//   divStable.appendChild(noteStable);
+//   divStable.style.backgroundColor ="#ff4500"
+//   divStable.style.border ="1px solid none"
+//   divStable.style.color ="whitesmoke"
+//   divStable.style.fontWeight ="400"
+//   divStable.style.borderRadius ="5px"
+//   divStable.style.padding ="7px"
+//   resultNote.appendChild(divStable)
+// }
+//  console.log(`yes: ${yes_count}, no:${no_count}`)
+//  getChart(yes_count,no_count)
+// })
 
-//  Drawing charts
-function getChart(yes_count,no_count){
-  google.charts.load('current', { 'packages': ['corechart'] });
-  google.charts.setOnLoadCallback(drawChart);
-  function drawChart(){
-    let data = google.visualization.arrayToDataTable([
-      ['Reply', 'Count',{ role: 'style' }],
-      ['Yes', yes_count,'#00f'],
-      ['No', no_count,'#f00']
-    ]);
+// //  Drawing charts
+// function getChart(yes_count,no_count){
+//   google.charts.load('current', { 'packages': ['corechart'] });
+//   google.charts.setOnLoadCallback(drawChart);
+//   function drawChart(){
+//     let data = google.visualization.arrayToDataTable([
+//       ['Reply', 'Count',{ role: 'style' }],
+//       ['Yes', yes_count,'#00f'],
+//       ['No', no_count,'#f00']
+//     ]);
 
-    const options = {
-      title: 'Policy Choice Stability Check',
-      width: 500,
-      height: 300,
-      legend: 'none',
-      bars: 'vertical',
-      hAxis: {
-        title: 'Replies'
-      },
-      vAxis: {
-        title: 'Count'
-      },
-    };
-  let chart = new google.visualization.ColumnChart(document.getElementById('chartContainer'));
-  chart.draw(data, options);
-  }
-}
+//     const options = {
+//       title: 'Policy Choice Stability Check',
+//       width: 500,
+//       height: 300,
+//       legend: 'none',
+//       bars: 'vertical',
+//       hAxis: {
+//         title: 'Replies'
+//       },
+//       vAxis: {
+//         title: 'Count'
+//       },
+//     };
+//   let chart = new google.visualization.ColumnChart(document.getElementById('chartContainer'));
+//   chart.draw(data, options);
+//   }
+// }
 
 
 
--- policyCheckProject/policyCheckProject/settings.py
+++ policyCheckProject/policyCheckProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gq26vp_9#47=kkuk0*-4b#ndkvrr1yyox$h18p*4qba8e1-4zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
