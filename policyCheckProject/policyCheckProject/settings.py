e1e452be2a1ee9586e3fefcdc7107a5699b2a691
OfomiMatthew <ofomimatthew7@gmail.com>
2023-08-19 17:23:53 +0100

    updated configuration

--- policyCheckProject/policyCheckProject/settings.py
+++ policyCheckProject/policyCheckProject/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'policyApp/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
